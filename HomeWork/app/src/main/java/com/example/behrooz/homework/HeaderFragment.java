ca9b9b783ad066453c0ed401a2ed5f829d762cbe
Arian <ariankholousi@gmail.com>
2017-12-24 13:57:38 +0330

    edit header fragment

--- HomeWork/app/src/main/java/com/example/behrooz/homework/HeaderFragment.java
+++ HomeWork/app/src/main/java/com/example/behrooz/homework/HeaderFragment.java
@@ -3,9 +3,11 @@ package com.example.behrooz.homework;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -13,17 +15,33 @@ import android.view.ViewGroup;
  */
 public class HeaderFragment extends Fragment {
 
+    public static final int REQ_ADD = 0;
+    public static final String ADD_DIALOG_TAG ="add_dialog_tag" ;
+    private ImageView imageView;
 
-  public HeaderFragment() {
-    // Required empty public constructor
-  }
+    public HeaderFragment() {
+        // Required empty public constructor
+    }
 
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_header, container, false);
-  }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_header, container, false);
+
+        imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                AddDialogFragment addDialogFragment = AddDialogFragment.newInstance();
+                addDialogFragment.setTargetFragment(HeaderFragment.this , REQ_ADD);
+                addDialogFragment.show(fragmentManager , ADD_DIALOG_TAG);
+            }
+        });
+
+        return view;
+    }
 
 }
