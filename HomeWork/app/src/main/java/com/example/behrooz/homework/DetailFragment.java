2f05fcbe7ab50a231508400797289ee1e00a7bad
Arian <ariankholousi@gmail.com>
2017-12-28 15:16:15 +0330

    Merge branch 'master' of C:\Users\Payami\Desktop\Dictionary with conflicts.

--- HomeWork/app/src/main/java/com/example/behrooz/homework/DetailFragment.java
+++ HomeWork/app/src/main/java/com/example/behrooz/homework/DetailFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.UUID;
 
@@ -19,15 +20,12 @@ import java.util.UUID;
  */
 public class DetailFragment extends Fragment {
 
-
-    public static final int REQ_DETAIL_FRAGMENT = 1 ;
     public static final String ARGS_UUID = "args_uuid";
     private Word word;
     private TextView tvWord;
     private TextView tvMeaning;
     private Button btnEdit;
     private Button btnRemove;
-
 
 
     public DetailFragment() {
@@ -39,21 +37,41 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail,container ,false);
 
-        word = (Word) getArguments().getSerializable(ARGS_UUID);
+        UUID wordUUid = (UUID) getArguments().getSerializable(ARGS_UUID);
+        word = WordRepository.getInstance(getActivity()).getWord(wordUUid);
 
         tvWord = (TextView) view.findViewById(R.id.tv_word);
         tvMeaning = (TextView) view.findViewById(R.id.tv_meaning);
         btnRemove = (Button) view.findViewById(R.id.btn_remove);
         btnEdit = (Button) view.findViewById(R.id.btn_edit);
 
+        tvWord.setText(word.getEnglishWord());
+        tvMeaning.setText(word.getPersianWord());
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WordRepository.getInstance(getActivity()).getWords().remove(word);
+                Toast.makeText(getActivity(), "Word successfully removed!", Toast.LENGTH_SHORT).show();
+                getFragmentManager().popBackStackImmediate();
+            }
+        });
+
 
         return view;
     }
 
 
-    public  static DetailFragment newInstance (UUID uuid){
+    public static DetailFragment newInstance(UUID uuid) {
         DetailFragment detailFragment = new DetailFragment();
 
         Bundle args = new Bundle();
